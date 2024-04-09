b5eddb5afd0c60ceab61cc618ffa0d6ae0c20021
Masoud Sam <masoud.sam@mtnirancell.ir>
2024-04-09 08:34:46 +0330

    add processing

--- src/renderer.js
+++ src/renderer.js
@@ -2,9 +2,12 @@
   const information = document.getElementById("info");
   const enable = document.getElementById("enable");
   const disable = document.getElementById("disable");
+  const process = document.getElementById("process");
 
   enable.addEventListener("click", async () => {
+    process.innerText = "Processing";
     await window.dns.enableShekan();
+    process.innerText = "";
     information.innerText = `Shekan is ON`;
     information.style.color = "green";
     enable.disabled = true;
@@ -12,14 +15,18 @@
   });
 
   disable.addEventListener("click", async () => {
+    process.innerText = "Processing";
     await window.dns.disableShekan();
+    process.innerText = "";
     information.innerText = `Shekan is OFF`;
     information.style.color = "red";
     enable.disabled = false;
     disable.disabled = true;
   });
 
+  process.innerText = "Processing";
   const response = await window.dns.isShekanDNS();
+  process.innerText = "";
   information.innerText = `Shekan is ${response ? "ON" : "OFF"}`;
   information.style.color = `${response ? "green" : "red"}`;
   enable.disabled = response;
