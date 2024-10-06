d4c322bc891fc87180f60239572dca90e72f90d1
Masoud Sam <mas.sam1368@gmail.com>
2024-10-06 17:35:29 +0330

    * refactor(utils.ts): remove unused debounce and throttle functions * chore(utils.ts): add eslint-disable-next-line comment to ignore import/prefer-default-export rule

--- src/renderer/utils.ts
+++ src/renderer/utils.ts
@@ -1,24 +1,4 @@
-export const debounce = (func: Function, delay: number) => {
-  let debounceTimer: any;
-  return function debouncedFunction(...args: any) {
-    clearTimeout(debounceTimer);
-    debounceTimer = setTimeout(() => func.apply(args), delay);
-  };
-};
-
-export const throttle = (func: Function, limit: number) => {
-  let inThrottle: boolean;
-  return function throttledFunction(...args: any) {
-    if (!inThrottle) {
-      func.apply(args);
-      inThrottle = true;
-      setTimeout(() => {
-        inThrottle = false;
-      }, limit);
-    }
-  };
-};
-
+// eslint-disable-next-line import/prefer-default-export
 export const extractTimeFromPingText = (text: string) => {
   const regex = /time=(\d+)ms/;
   const match = text?.match(regex);
